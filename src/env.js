b8797fec2304afd2fcb7b2522b6040bc5c7ea0a3
ghostnetrn <54438080+ghostnetrn@users.noreply.github.com>
2021-07-04 14:21:19 -0300

    update

---
+++ src/env.js
@@ -0,0 +1,31 @@
+// read .env file into proces.env
+require("dotenv").config()
+
+const envalid = require("envalid")
+var pjson = require("../package.json")
+
+module.exports = envalid.cleanEnv(process.env, {
+    apiKey: envalid.str(),
+    apiSecret: envalid.str(),
+    BOT_TOKEN: envalid.str(),
+    BOT_CHAT: envalid.str(),
+    botId: envalid.str({ default: "bot_1" }),
+	montante: envalid.num({ default: "300.00" }),
+	valorInicial: envalid.num({ default: "300.00" }),
+	moedaCorrente: envalid.str({ default: "BRL" }),
+	dataInicial: envalid.str({ default: "01/01/2021" }),
+	minPercentualLucro: envalid.num({ default: "0.03" }),
+	intervalo: envalid.num({ default: "2.5" }),
+	host1: envalid.host({ default: "143.198.237.4" }),
+    host2: envalid.host({ default: "64.227.99.7" }),
+    port: envalid.port({
+        default: 80,
+        desc: "The port to start the server on",
+    }),
+    multibot: envalid.bool({ default: true }),
+    TRADER_PORT: envalid.port({
+        default: 8003,
+        desc: "The port to trader webserver runs",
+    }),
+    VERSION: envalid.str({ default: pjson.version }),
+})
