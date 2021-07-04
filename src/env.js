c921591ea0c263798ed13d408a4995e451a02588
ghostnetrn <54438080+ghostnetrn@users.noreply.github.com>
2021-07-04 15:28:51 -0300

    update

--- src/env.js
+++ src/env.js
@@ -5,14 +5,19 @@ const envalid = require("envalid")
 var pjson = require("../package.json")
 
 module.exports = envalid.cleanEnv(process.env, {
-    apiKey: envalid.str(),
-    apiSecret: envalid.str(),
-    BOT_TOKEN: envalid.str(),
-    BOT_CHAT: envalid.str(),
+    apiKey: envalid.str( {default: ""}),
+    apiSecret: envalid.str( {default: ""}),
+	apiKeyBinance: envalid.str( {default: ""}),
+    apiSecretBinance: envalid.str( {default: ""}),
+	depositoUSDT: envalid.num({ default: "30.00" }),
+	dataInicialBinance: envalid.str({ default: "01/01/2021" }),
+    BOT_TOKEN: envalid.str( {default: ""}),
+    BOT_CHAT: envalid.str( {default: ""}),
     botId: envalid.str({ default: "bot_1" }),
 	montante: envalid.num({ default: "300.00" }),
 	valorInicial: envalid.num({ default: "300.00" }),
 	moedaCorrente: envalid.str({ default: "BRL" }),
+	moedaAcumular: envalid.str({ default: "BRL" }),
 	dataInicial: envalid.str({ default: "01/01/2021" }),
 	minPercentualLucro: envalid.num({ default: "0.03" }),
 	intervalo: envalid.num({ default: "2.5" }),
