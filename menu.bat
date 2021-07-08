3426b23c9d9b3a194c3d34ec18339fa3a80d856d
ghostnetrn <54438080+ghostnetrn@users.noreply.github.com>
2021-07-08 09:58:12 -0300

    update

--- menu.bat
+++ menu.bat
@@ -3,6 +3,6 @@ cls
 set /p nomeVar=Digite um nome:
 echo Voce digitou %nomeVar%
 REM heroku login
-heroku git:remote -a %nomeVar%
+heroku git:remote -a %nomeVar% 
 REM git push heroku master
 REM heroku logs --tail
